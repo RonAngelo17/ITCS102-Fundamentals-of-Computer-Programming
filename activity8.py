fc6914d5186c1948813b1e7229f69421239e881a
Ron Angelo Rosel <ronangelooo17@gmail.com>
2025-08-23 18:47:18 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,12 @@
+a = 17
+b = 7
+
+name1 = 'Ron'
+name2 = 'Angelo' 
+
+print( a>=b )
+print( name1 == name2 )
+print( name1 != name2 )
+
+
+
---
+++ activity9.py
@@ -0,0 +1,10 @@
+a = 7
+b = 2
+
+c = 9
+d = 1
+
+
+print ( (a > b) and (c < d) )
+print ( (a > b) or (c < d) )
+print (not ((a < b) or (c < d)) )
